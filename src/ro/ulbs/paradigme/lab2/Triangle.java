ea928fa7238c4c6705574b653e400282365c7363
Anca-Izabela <tobiasizabela2003@gmail.com>
2025-03-07 11:21:38 +0200

    primul commit

---
+++ src/ro/ulbs/paradigme/lab2/Triangle.java
@@ -0,0 +1,39 @@
+package ro.ulbs.paradigme.lab2;
+
+public class Triangle extends Form {
+    private float height;
+    private float base;
+
+    public Triangle() {
+        super();
+        this.height = 0;
+        this.base = 0;
+    }
+
+    public Triangle(float height, float base, String color) {
+        super(color);
+        this.height = height;
+        this.base = base;
+    }
+
+    @Override
+    public float getArea() {
+        return (base * height) / 2;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " Triangle with height: " + height + " and base: " + base;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        Triangle triangle = (Triangle) obj;
+        return Float.compare(triangle.height, height) == 0 &&
+                Float.compare(triangle.base, base) == 0 &&
+                this.getColor().equals(triangle.getColor());
+    }
+}
